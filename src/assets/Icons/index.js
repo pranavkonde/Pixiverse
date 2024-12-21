2749352815736a2f423c85d452c29dd6bbdb5d11
Pranav Konde <76070589+pranavkonde@users.noreply.github.com>
2024-12-22 00:27:59 +0530

    Add assets and Icons

---
+++ src/assets/Icons/index.js
@@ -0,0 +1,15 @@
+import LockIcon from "./lock.svg";
+import PixiverseName from "./pixiverse.svg";
+import MusicIcon from "./music.svg";
+import MusicOffIcon from "./musicOff.svg";
+import LabelIcon from "./label.svg";
+import CopyIcon from "./copy.svg";
+
+export {
+  LockIcon,
+  PixiverseName,
+  MusicIcon,
+  MusicOffIcon,
+  LabelIcon,
+  CopyIcon,
+};
